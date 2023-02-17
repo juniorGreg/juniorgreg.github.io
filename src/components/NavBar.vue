c7ef43ad656a466884dda7ce049ca53a0ab7e4db
Junior Gregoire <junior.gregoire@gmail.com>
2023-02-17 01:28:05 -0500

    Tweak styles and animations

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,12 +7,26 @@
 </script>
 
 <template>
-  <nav class="fixed top-0 left-0 bg-blue-500/50 w-full ">
+  <nav class="fixed top-0 left-0 bg-blue-500/50 w-full animation-accordion">
     <div class="container mx-auto flex space-x-3 py-1">
-      <a v-for="id in sectionsIds" :href="`#${id}`" class="underline hover:decoration-4">
+      <a v-for="id in sectionsIds" :href="`#${id}`" class="underline hover:decoration-4 font-medium">
          {{ id }}
       </a>
     </div>
   </nav>
 </template>
+
+<style>
+
+  @keyframes a-animation {
+    from {opacity: 0;}
+    to {opacity: 1;}
+  }
+
+  a {
+    animation-name: a-animation;
+    animation-duration: 2s;
+  }
+
+</style>
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+    extend: {
+
+      animation: {
+        accordion: "accordion 2s ease-in forwards"
+      },
+
+      keyframes: {
+        accordion: {
+          "0%": { height: 0%; }
+          "100%" { height: 32px; }
+        }
+      }
+    }
+    
 	},
 	plugins: [],
 }
